64aa985ea0779cd60de1042207e6ef879c9a16d9
quiz-rndm <kayyjpn@gmail.com>
2024-12-05 08:08:38 +0700

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,83 +1,83 @@
 let questions = [{
         numb: 1,
-        question: "Apa warna kerusakaan gua?",
-        answer: "Biru",
+        question: "1+1 ?",
+        answer: "Semua benar",
         options: [
-            "Hijau",
-            "Biru Tua",
-            "Biru muda",
-            "Biru"
+            "2",
+            "= 2",
+            "Jendela",
+            "Semua benar"
         ]
     },
     {
         numb: 2,
-        question: "Kapan gua ultah?",
-        answer: "23 Juni",
+        question: "Gua lahir dimana?",
+        answer: "Jakarta Timur",
         options: [
-            "23 Juli",
-            "23 Juni",
-            "23 April",
-            "23 November"
+            "Jakarta Timur",
+            "Bekasi",
+            "Jakarta Pusat",
+            "Wonogiri"
         ]
     },
     {
         numb: 3,
-        question: "Gua kelahiran taun berapa?",
-        answer: "2008",
+        question: "Nama wa gua?",
+        answer: "kkeikoo",
         options: [
-            "2007",
-            "2006",
-            "2005",
-            "2008"
+            "keiko",
+            "kkeikoo",
+            "kkeikooo",
+            "Semua salah"
         ]
     },
     {
         numb: 4,
-        question: "Apa hobby gua??",
-        answer: "Bola",
+        question: "Redistribute digunakan untuk?",
+        answer: "Menghubungkan routing protokol yang berbeda dalam 1 topologi",
         options: [
-            "Coding",
-            "Bola",
-            "Futsal",
-            "Bulu Tangkis"
+            "Membatasi lalu lintas jaringan",
+            "Menghubungkan routing protokol yang berbeda dalam 1 topologi",
+            "Mengurangi jumlah router yang terlibat dalam proses routing",
+            "Semua salah"
         ]
     },
     {
         numb: 5,
-        question: "Apa makanan kesukaan gua?",
-        answer: "Mie & Maklor",
+        question: "Ada bahasa apa aja di web ini?",
+        answer: "HTML, CSS, JAVA SCRIPT",
         options: [
-            "Telor & Cumi",
-            "Mie & Maklor",
-            "Nasgor & Mie",
-            "Sate & Bakso"
+            "HTML, CSS",
+            "HTML, JAVA, CSS",
+            "HTML, PYTHON, JAVA SCRIPT",
+            "HTML, CSS, JAVA SCRIPT"
         ]
     },
     {
         numb: 6,
-        question: "Dijurusan apa gua sekarang?",
-        answer: "TKJ",
+        question: "Nama ig gua dulu?",
+        answer: "kayhnz_",
         options: [
-            "TKJ",
-            "MP",
-            "AK",
-            "RPL"
+            "kkeikoo_",
+            "kayhnz_",
+            "kayy_",
+            "kkeiko_"
         ]
     },
-    {
+        {
         numb: 7,
-        question: "Nama hospot gua?",
-        answer: "1+1 =",
+        question: "Nama mobile legends gua?",
+        answer: "Semua salah",
         options: [
-            "1+1 =",
-            "Samalekom",
-            "Apaja",
-            "Kay123"
+            "kkeikoo_",
+            "kayhnz_",
+            "kkeikoo",
+            "Semua salah"
         ]
     },
-    {
+            {
         numb: 8,
-        question: "Sandi hospot gua?",
+        question: "sandi hospot gua?",
         answer: "sama dengan d2a",
         options: [
             "sama dengan d2a",
